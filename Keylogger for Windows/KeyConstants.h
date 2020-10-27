aedcca0d92e742d39936ba0a3a0d9974ac4c716b
Ishantha Udara <ishanthaudara96@gmail.com>
2020-10-27 13:05:10 +0530

    class keypair

--- Keylogger for Windows/KeyConstants.h
+++ Keylogger for Windows/KeyConstants.h
@@ -1,3 +1,16 @@
 #pragma once
+#ifndef KEYCONSTANTS_H
+#define KEYCONSTANTS_H
+
+#include <map>
+#include <string>
+
+class KeyPair {
+public:
+	KeyPair	(const std::string &vk = "", const std::string &name = "")
+};
+
+
+#endif // !KEYCONSTANTS_H
 
 
