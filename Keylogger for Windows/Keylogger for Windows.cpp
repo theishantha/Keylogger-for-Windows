235494a4217858d7d2bf47b939d18043234da1d7
Ishantha Udara <ishanthaudara96@gmail.com>
2020-10-27 11:13:52 +0530

    main.cpp init

--- Keylogger for Windows/Keylogger for Windows.cpp
+++ Keylogger for Windows/Keylogger for Windows.cpp
@@ -1,15 +1,15 @@
 // Keylogger for Windows.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-
-using namespace std
-
-int main()
-{
-	cout << 
-
-}
+//#include <iostream>
+//
+//using namespace std
+//
+//int main()
+//{
+//	cout << 
+//
+//}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
