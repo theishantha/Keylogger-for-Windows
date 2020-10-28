ecf16eff62ff7b65ac6c5f18c624c2a21a362286
Ishantha Udara <ishanthaudara96@gmail.com>
2020-10-28 20:51:34 +0530

    28 Oct

--- Keylogger for Windows/Helper.h
+++ Keylogger for Windows/Helper.h
@@ -1,1 +1,22 @@
-#pragma once
+
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <ctime>
+#include <string>
+#include <sstream>
+
+namespace Helper {
+	template <class T> 
+
+	std::string ToString(const T&);
+
+	struct DateTime
+	{
+
+	};
+}
+
+
+#endif // !HELPER_H
+
