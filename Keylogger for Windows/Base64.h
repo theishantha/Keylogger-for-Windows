8c26b9087abf2a91204a5704e5914c9392ac3858
Ishantha Udara <ishanthaudara96@gmail.com>
2020-10-29 21:11:27 +0530

    salting

--- Keylogger for Windows/Base64.h
+++ Keylogger for Windows/Base64.h
@@ -6,6 +6,13 @@
 #include <vector>
 #include <string>
 
+namespace Base64 {
+	std::string base64_encode(const std::string &);
+
+	const std::string &SALT1 = "LM::TB::BB";
+	const std::string &SALT2 = "_:/_77";
+	const std::string &SLAT3 = "line=wowC++";
+}
 
 
 
